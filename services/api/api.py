4307177a321b9ea17e3871fec1779d8ed742e5e1
Christian Raue <captain_2@gmx.de>
2026-04-01 12:48:50 +0200

    basic communication works

--- services/api/api.py
+++ services/api/api.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Request
 from endpoints import router
 import debug
 import events
@@ -18,5 +19,19 @@ if debug.in_debug_mode():
 else:
     app = FastAPI(lifespan=lifespan, docs_url=None, redoc_url=None, openapi_url=None)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust as needed
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods, including OPTIONS
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(router)
+
+
+@app.middleware("http")
+async def log_request_body(request: Request, call_next):
+    body = await request.body()
+    print("Raw request body:", body.decode())
+    return await call_next(request)
